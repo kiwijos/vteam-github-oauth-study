6c074e460da7e50070a365927df0d75c9479abe1
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-11-27 20:37:03 +0100

    Add conditionl logic to display signed in user and logout button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
 	import { PUBLIC_GITHUB_ID } from '$env/static/public';
+	import { page } from '$app/stores';
 </script>
 
-<p>
-	Sign in using <a
-		href="https://github.com/login/oauth/authorize?scope=user:email&client_id={PUBLIC_GITHUB_ID}"
-		>GitHub</a
-	>
-</p>
+{#if $page.data.user}
+	<p>Signed in as {$page.data.user.name}</p>
+	<form action="/logout" method="POST">
+		<button type="submit">Log out</button>
+	</form>
+{:else}
+	<p>
+		Sign in using <a
+			href="https://github.com/login/oauth/authorize?scope=user:email&client_id={PUBLIC_GITHUB_ID}"
+			>GitHub</a
+		>
+	</p>
+{/if}
